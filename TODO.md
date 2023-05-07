ffeebbd4349ae7fd9dd9adffa2523ada51ae75de
matteotolloso <matteotolloso@gmail.com>
2023-05-07 14:55:17 +0200

    experiments on classification
    Co-authored-by: Irene Testa <iretes@users.noreply.github.com>
    Co-authored-by: AndreaAlberti116 <AndreaAlberti116@users.noreply.github.com>

---
+++ TODO.md
@@ -0,0 +1,9 @@
+# cose da fare oggi
+
+- salvataggio dei data con scvi
+- applicare ad altri dataset
+- cercare algoritmi specifici per ogni tool
+- analisi dei nan ottenuti nel clustering
+- classificazione
+- come organizzare il report?
+- enrichment analysis?
--- classificatore.py
+++ classificatore.py
@@ -5,7 +5,9 @@ from sklearn.metrics import f1_score
 import scanpy as sc
 import numpy as np
 
-MARKERS_PATH = "./results/markers_10X_P7_4_seurat.csv"
+#MARKERS_PATH = "./results/markers_10X_P7_4_seurat.csv"
+MARKERS_PATH = './results/draft_rank_genes_scanpy.csv'
+
 LABELS_PATH = "./filtered_dataset/tabulamuris/labels_10X_P7_4.csv"
 markers_df = pd.read_csv(MARKERS_PATH)  
 top_genes = markers_df.gene.unique()
@@ -16,7 +18,8 @@ adata = sc.read_10x_mtx(
     cache=False
 )
 
-X = adata[:, top_genes].X.toarray()
+#X = adata[:, top_genes].X.toarray()
+X=adata.X.toarray()
 labels_df = pd.read_csv(LABELS_PATH)
 # y = labels_df['cluster.ids'].to_numpy()
 
@@ -29,15 +32,30 @@ a = np.isnan(y).reshape(-1)
 y = y[a == False]
 X = X[~a, :]
 
- 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, stratify=y)
+from sklearn.metrics import classification_report
+from sklearn.model_selection import cross_val_predict
+
 clf = RandomForestClassifier()
-clf.fit(X_train, y_train)
-y_pred = clf.predict(X_test)
-print(f1_score(y_test, y_pred, average='macro'))
+y_pred = cross_val_predict(clf, X, y, cv=5)
+report = classification_report(y, y_pred, output_dict=True)
+clf.fit(X, y)
+feature_importance = clf.feature_importances_ # calcolare importances di ciascun fold e aggregare i rankings?
 
-importance = clf.feature_importances_
+all_features_list = adata.var_names
 
-a = 1
-# Read the data
-#data = pd.read_csv('./scanpy/.csv')
+# sort both lists by feature_importance mantaining the same order
+sorted_list = sorted(zip(feature_importance, all_features_list), reverse=True)
+
+# top 120 genes with feature importance different from all the others
+
+current_score = sorted_list[120][0]
+top_sorted_list = [x[1] for x in sorted_list if x[0] >= current_score]
+intersection = set(top_genes).intersection(set(top_sorted_list))
+
+
+
+# all genes
+# {'precision': 0.942666699684296, 'recall': 0.9415041782729805, 'f1-score': 0.9404974321493618, 'support': 359}
+# top
+# {'precision': 0.9700833699346517, 'recall': 0.9693593314763231, 'f1-score': 0.9693161075050654, 'support': 359}
+print('fine')
