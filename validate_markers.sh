b382715f96c51d7bcb41d87713a2b6d2bf8a802f
Irene Testa <testa.irene1509@gmail.com>
2023-05-10 22:52:57 +0200

    recursive feature elimination

--- validate_markers.sh
+++ validate_markers.sh
@@ -3,12 +3,12 @@
 # ---- TABULA MURIS ----
 
 # Seurat
+# TODO: change paths
 python ./markers_validation.py \
 --data-path "./filtered_dataset/tabulamuris/data_10X_P7_4/" \
 --markers-path "./results/markers_10X_P7_4_seurat.csv" \
 --labels-path "./filtered_dataset/tabulamuris/labels_10X_P7_4.csv" \
 --out-path "./results/" \
---tool "seurat"
 
 # Scanpy
 # ...
