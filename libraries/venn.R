866023a1639ad8bd679606b39acba1af37b76fe5
Irene Testa <testa.irene1509@gmail.com>
2023-05-18 16:23:58 +0200

    robe

--- libraries/venn.R
+++ libraries/venn.R
@@ -5,20 +5,15 @@ plot_venn = function(markers, out_dir) {
   cluster_ids = unique(markers$cluster)
   for (i in cluster_ids){
     cluster_markers = markers[markers$cluster == i,]
-    #setEPS()
-    #pdf(file=paste(out_dir,"cluster", i, "_markers_size_venn.pdf",sep=""))
-    #postscript(paste(out_dir,"cluster", i, "_markers_size_venn.eps",sep=""))
+    pdf(file=paste(out_dir,"cluster", i, "_markers_size_venn.pdf",sep=""))
     venn(
       split(cluster_markers$gene, cluster_markers$tool), 
-      zcolor = "style",
+      zcolor = c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442"),
       box = FALSE
       )
     dev.off()
   }
-  # TODO
-  #setEPS()
-  #postscript(paste(out_dir,"markers_size_venn.eps",sep=""))
   pdf(file=paste(out_dir,"markers_size_venn.pdf",sep=""))
-  venn(split(markers$gene, markers$tool), box = FALSE, zcolor = "style")
+  venn(split(markers$gene, markers$tool), box = FALSE, zcolor = c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442"))
   dev.off()
 }
